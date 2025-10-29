d2fd0b36a29150ee56857c0c91174fb6a3266f7d
Vishvesh-Shrikant <vishveshshri@gmail.com>
2025-10-29 22:21:26 +0530

    changes

--- ai-lab-project-server/index.js
+++ ai-lab-project-server/index.js
@@ -7,7 +7,12 @@ config();
 const app = express();
 
 // --- Middleware ---
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json()); // Body parser for JSON
 
 // --- DB Config ---
